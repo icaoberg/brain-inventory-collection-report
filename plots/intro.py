ed619fdf2689e127c3586a4e4d85c20925a8436a
icaoberg <icaoberg@cmu.edu>
2025-07-25 08:12:43 +0000

    Fixed title

--- plots/intro.py
+++ plots/intro.py
@@ -16,7 +16,7 @@ def print_collection_intro():
     st.markdown(f"### 📅 Report Date: {today_str}")
 
 def print_dataset_intro():
-    st.title("🧠 Brain Image Library Inventory Datasets")
+    st.title("🧠 Brain Image Library Datasets")
 
     st.markdown(
         """
