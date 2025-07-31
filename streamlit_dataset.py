7bb587c9e98244030a0361293a52a149e04a02a0
icaoberg <icaoberg@cmu.edu>
2025-07-31 20:10:40 +0000

    Removed error

--- streamlit_dataset.py
+++ streamlit_dataset.py
@@ -247,10 +247,7 @@ try:
             )
 
         with st.expander("📄 Datacite"):
-            try:
-                st.json(json.loads(brainzzz.dois.__get_datacite_metadata(dataset_id=selected_bildid)))
-            except:
-                st.error(f"❌ Failed to load or process data from Datacite.")
+            st.json(json.loads(brainzzz.dois.__get_datacite_metadata(dataset_id=selected_bildid)))
 
         with st.expander("📊 Useful Plots"):
             plot_extension_histogram(manifest_df)
